a210b1c26a436d66b5d37eba639b92ba07034efa
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-07-04 17:13:37 +0900

    make the project of Instagram ui

---
+++ Instagram_profile/Instagram_profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram_profile
+//
+//  Created by 宮本一成 on 2020/07/04.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
